825496ce2c9adbd9ed4f199ec4edadc82edfc623
stephcerratti <stephanie.cerratti@gmail.com>
2018-10-25 21:02:10 -0400

    initial checklist components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './assets/bridge-logo.svg';
 import { checkApiServer } from './api';
 import Header from './components/Header';
+import CheckList from './components/CheckList';
 
 class App extends Component {
 	componentDidMount() {
@@ -15,6 +16,7 @@ class App extends Component {
 				<p className="App-intro">
 					To get started, edit <code>src/App.js</code> and save to reload.
 				</p>
+				<CheckList />
 			</div>
 		);
 	}
---
+++ src/components/CheckList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CheckListItem from "./CheckListItem.js";
+
+const CheckList = props => {
+	return (
+		<React.Fragment>
+			<h2> Company Checklist</h2>
+			<CheckListItem />
+		</React.Fragment>
+		)
+}
+
+export default CheckList;
